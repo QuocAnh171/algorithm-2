216a55ae0d6d0fbc0ee40ec819915978773d3a3d
QuocAnh171 <74640069+QuocAnh171@users.noreply.github.com>
2020-11-23 14:04:31 +0700

    Update alth1.py

--- alth1.py
+++ alth1.py
@@ -5,24 +5,6 @@ while x >= y :
     y = int(input())
 # print(x,y)
 count = 0
-# a = y//213
-# a1 = a+1
-# b = y//3
-# c = y//4
-# while x != y:
-#         if x < c:
-#             x *= 2
-#             count += 1
-#         elif c <= x < b:
-#             x -= 1
-#             count += 1
-#         elif b <= x <= (a+1):
-#             x *= 2
-#             count += 1
-#         else:
-#             x -= 1
-#             count += 1
-# print(count)
 while x != y:
     z = y
     while x < z :
